bd59f52186ba5887ed0bb4e2cf3ca4bbfe20eafb
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-01-24 22:43:12 -0600

    added Hz values to transfer units since sometimes memory is measured in Hz instead of T/s

--- sqitch/deploy/transfer_units.sql
+++ sqitch/deploy/transfer_units.sql
@@ -3,10 +3,15 @@
 BEGIN;
 
 CREATE TYPE transfer_units AS enum (
+    'Hz',
     'T/s',  -- 1 transfer per second
+    'KHz',
     'KT/s', -- 1,000 transfers per second
+    'MHz',
     'MT/s', -- 1,000,000 transfers per second
+    'GHz',
     'GT/s', -- 1,000,000,000 transfers per second
+    'THz',
     'TT/s', -- 1,000,000,000,000 transfers per second
     'PT/s'  -- 1,000,000,000,000,000 transfers per second
 );
