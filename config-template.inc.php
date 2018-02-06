1bfbf1d93e66bdcaadd125bd0e8d2db966533a83
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-02-05 23:45:18 -0600

    add additional possible options to template

--- config-template.inc.php
+++ config-template.inc.php
@@ -5,9 +5,16 @@ $dbconfig = array();
 $dbconfig['pos']['dsn'] = 'mysql:dbname=unicentaopos;host=localhost'; 
 $dbconfig['pos']['username'] = 'unicentaopos';
 $dbconfig['pos']['password'] = 'password';
-$dbconfig['pos']['options'] = null;
+$dbconfig['pos']['options'] = array(PDO::ATTR_PERSISTENT => true);
 
 $dbconfig['auth']['dsn'] = 'mysql:dbname=osticket;host=localhost';
 $dbconfig['auth']['username'] = 'osticket';
 $dbconfig['auth']['password'] = 'password';
-$dbconfig['auth']['options'] = null;
+$dbconfig['auth']['options'] = array(PDO::ATTR_PERSISTENT => true);
+
+$default_print_queue = 'remote-QL-570';
+
+//$printconfig = array();
+//$printconfig['pos']['type'] = 'escpos';
+
+//$default_print_queue = 'pos';
