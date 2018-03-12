89fb27cb784c3fc8fb4c105e24114d5ead2a8b34
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-03-12 00:10:48 -0500

    fixing how globals are used and included

--- include/Auth.php
+++ include/Auth.php
@@ -2,10 +2,10 @@
 
 class Auth {        
     function __construct() {
-        // Connect to POS and authorization databases
-        include_once 'config.inc.php';
+        // Connect to POS and authorization databases      
         
         try {
+            global $dbconfig;
             $this->authdb = new PDO($dbconfig['auth']['dsn'], $dbconfig['auth']['username'], $dbconfig['auth']['password'], $dbconfig['auth']['options']);
         } catch(PDOException $e) {
             echo 'Error connecting to auth DB. Caught exception: [',  $e->getMessage(), "]\n";
--- include/default.inc.php
+++ include/default.inc.php
@@ -1,30 +1,29 @@
 <?php
-{
-    header("Cache-Control: max-age=0,no-cache,no-store,post-check=0,pre-check=0");
-    
-    error_reporting(E_ALL);
-    // Disable on production copy of code
-    ini_set("display_errors", "On");
-    
-    spl_autoload_register(function ($class_name) {
-        include 'include/' . $class_name . '.php';
-    });
-    
-    ini_set("session.cookie_lifetime", 0);
-    ini_set("session.use_cookies", "On"); 
-    ini_set("session.use_only_cookies", "On"); 
-    ini_set("session.use_strict_mode", "On"); 
-    ini_set("session.cookie_httponly", "On"); 
-    ini_set("session.cookie_secure", "On"); 
-    ini_set("session.use_trans_sid", "Off"); 
-    ini_set("session.cache_limiter", "nocache"); 
-    ini_set("session.sid_length", "48"); 
-    ini_set("session.sid_bits_per_character", "6"); 
-    ini_set("session.hash_function", "sha256"); 
-    
-    include_once 'include/config.inc.php';
-}
 
+header("Cache-Control: max-age=0,no-cache,no-store,post-check=0,pre-check=0");
+
+error_reporting(E_ALL);
+
+// Disable on production copy of code
+ini_set("display_errors", "On");
+ini_set("session.cookie_lifetime", 0);
+ini_set("session.use_cookies", "On"); 
+ini_set("session.use_only_cookies", "On"); 
+ini_set("session.use_strict_mode", "On"); 
+ini_set("session.cookie_httponly", "On"); 
+ini_set("session.cookie_secure", "On"); 
+ini_set("session.use_trans_sid", "Off"); 
+ini_set("session.cache_limiter", "nocache"); 
+ini_set("session.sid_length", "48"); 
+ini_set("session.sid_bits_per_character", "6"); 
+ini_set("session.hash_function", "sha256"); 
+    
+include_once 'include/config.inc.php';
+
+spl_autoload_register(function ($class_name) {
+    include 'include/' . $class_name . '.php';
+});
+    
 function showError($msg) {
     if ($msg) {
         echo '<div class=error>'.$msg.'</div>';
