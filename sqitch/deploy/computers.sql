09e65ef463a411361c59ad50de0871dadb046c26
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-03-25 09:42:30 -0500

    working to add delete rules to clean up manufacturers after removing rows from computers

--- sqitch/deploy/computers.sql
+++ sqitch/deploy/computers.sql
@@ -85,41 +85,65 @@ CREATE rule update_v_computers AS ON UPDATE TO v_computers DO INSTEAD (
     insert into manufacturers(name) values (NEW.system_manufacturer) on conflict do nothing;
     insert into manufacturers(name) values (NEW.baseboard_manufacturer) on conflict do nothing;
     insert into manufacturers(name) values (NEW.chassis_manufacturer) on conflict do nothing;
-    update computers SET (system_manufacturer,
-                            system_product_name,
-                            system_version,
-                            system_sn,
-                            baseboard_manufacturer,
-                            baseboard_product_name,
-                            baseboard_version,
-                            baseboard_sn,
-                            baseboard_asset_tag,
-                            chassis_manufacturer,
-                            chassis_type,
-                            chassis_version,
-                            chassis_sn,
-                            chassis_asset_tag,
-                            os,
-                            barcode) = (      
-        (SELECT manufacturer_id FROM manufacturers WHERE NEW.system_manufacturer = name),
-        NEW.system_product_name,
-        NEW.system_version,
-        NEW.system_sn,
-        (SELECT manufacturer_id FROM manufacturers WHERE NEW.baseboard_manufacturer = name),
-        NEW.baseboard_product_name,
-        NEW.baseboard_version,
-        NEW.baseboard_sn,
-        NEW.baseboard_asset_tag,
-        (SELECT manufacturer_id FROM manufacturers WHERE NEW.chassis_manufacturer = name),
-        NEW.chassis_type,
-        NEW.chassis_version,
-        NEW.chassis_sn,
-        NEW.chassis_asset_tag,
-        NEW.os,
-        NEW.barcode      
-    ) WHERE computer_id = OLD.computer_id;
+    update computers SET system_manufacturer = COALESCE((SELECT manufacturer_id FROM manufacturers WHERE NEW.system_manufacturer = name), system_manufacturer),
+    					 system_version = COALESCE(NEW.system_version, OLD.system_version),
+                         system_sn = COALESCE(NEW.system_sn, OLD.system_sn),
+                         baseboard_manufacturer = COALESCE((SELECT manufacturer_id FROM manufacturers WHERE NEW.baseboard_manufacturer = name), baseboard_manufacturer),
+                         baseboard_product_name = COALESCE(NEW.baseboard_product_name, OLD.baseboard_product_name),
+                         baseboard_version = COALESCE(NEW.baseboard_version, OLD.baseboard_version),
+                         baseboard_sn = COALESCE(NEW.baseboard_sn, OLD.baseboard_sn),
+                         baseboard_asset_tag = COALESCE(NEW.baseboard_asset_tag, OLD.baseboard_asset_tag),
+                         chassis_manufacturer = COALESCE((SELECT manufacturer_id FROM manufacturers WHERE NEW.chassis_manufacturer = name), chassis_manufacturer),
+                         chassis_type = COALESCE(NEW.chassis_type, OLD.chassis_type),
+                         chassis_version = COALESCE(NEW.chassis_version, OLD.chassis_version),
+                         chassis_sn = COALESCE(NEW.chassis_sn, OLD.chassis_sn),
+                         chassis_asset_tag = COALESCE(NEW.chassis_asset_tag, OLD.chassis_asset_tag),
+                         os = COALESCE(NEW.os, OLD.os),
+                         barcode = COALESCE(NEW.barcode, barcode)
+	WHERE computer_id = OLD.computer_id AND
+		  (OLD.system_manufacturer <> NEW.system_manufacturer OR
+		   OLD.system_version <> NEW.system_version OR
+		   OLD.system_sn <> NEW.system_sn OR
+		   OLD.baseboard_manufacturer <> NEW.baseboard_manufacturer OR
+		   OLD.baseboard_product_name <> NEW.baseboard_product_name OR
+		   OLD.baseboard_version <> NEW.baseboard_version OR
+		   OLD.baseboard_asset_tag <> NEW.baseboard_asset_tag OR
+		   OLD.chassis_manufacturer <> NEW.chassis_manufacturer OR
+		   OLD.chassis_type <> NEW.chassis_type OR
+		   OLD.chassis_version <> NEW.chassis_version OR
+		   OLD.chassis_sn <> NEW.chassis_sn OR
+		   OLD.chassis_asset_tag <> NEW.chassis_asset_tag OR
+		   OLD.os <> NEW.os OR
+		   OLD.barcode <> NEW.barcode)
+		   
 );
 
+CREATE OR REPLACE FUNCTION delete_computers(id bigint) RETURNS void AS $$
+DECLARE
+    old_c computers%ROWTYPE;
+
+BEGIN
+  BEGIN
+        SELECT system_manufacturer, baseboard_manufacturer, chassis_manufacturer FROM computers INTO old_c WHERE computer_id=id;
+
+        DELETE FROM computers
+        WHERE computer_id = id;
+  END;
+
+  BEGIN
+    DELETE FROM manufacturers
+     WHERE manufacturer_id = old_c.system_manufacturer OR
+           manufacturer_id = old_c.baseboard_manufacturer OR
+           manufacturer_id = old_c.chassis_manufacturer;
+EXCEPTION
+        WHEN foreign_key_violation THEN --
+END;
+
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE RULE delete_v_computers AS ON DELETE TO v_computers
+	DO INSTEAD (PERFORM delete_computers(OLD.computer_id));
 
 -- Read-Only user can select
 GRANT SELECT ON computers TO inventory_ro;
