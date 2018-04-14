ae6b81da115490ecdc8e8085b0b1fd7dd5d5bdc5
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-04-14 03:15:16 -0500

    added hosts table to record state of an OS host at time system is inventoried

--- sqitch/deploy/hosts.sql
+++ sqitch/deploy/hosts.sql
@@ -4,7 +4,14 @@ BEGIN;
 
 -- XXX Add DDLs here.
 
-CREATE TABLE hosts (hostname text, computer_id bigint references computers, ip inet, updated_at timestamptz);
+CREATE TABLE hosts (host_id bigserial PRIMARY KEY, fqdn text, hostname text NOT NULL, computer_id bigint references computers NOT NULL, ip inet, updated_at timestamptz default now());
 
+-- Read-Only user can select
+GRANT SELECT ON hosts TO inventory_ro;
+GRANT SELECT ON SEQUENCE hosts_host_id_seq TO inventory_ro;
+
+-- Read-Write user can insert/update/delete and manipulate the sequence
+GRANT SELECT,INSERT,UPDATE,DELETE ON hosts TO inventory_rw;
+GRANT USAGE ON SEQUENCE hosts_host_id_seq TO inventory_rw;
 
 COMMIT;
